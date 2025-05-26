294217abc86e6d001ede6fc42c6d5b41dd17971e
leokingue <ebangkingue@icloud.com>
2025-05-26 13:31:16 +0200

    Add the Customer MicroService and provide the remote configuration on Config Server

---
+++ services/customer/src/test/java/com/kingue/ecommerce/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingue.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
